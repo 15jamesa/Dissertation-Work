e612bce40114b72e57caf15b9cbca0c68e8f047c
Ava <69300596+15jamesa@users.noreply.github.com>
2025-03-05 13:04:56 +0000

    Fixing bug and forming H from h_hat

--- Syndrome-Trellis Code/STC.jl
+++ Syndrome-Trellis Code/STC.jl
@@ -1,7 +1,10 @@
+using SparseArrays
+
 function embed(h_hat, x, m, rho)
+    #reads columns upwards
     #forward step of viterbi
     h,w = size(h_hat)
-    block_num = 4
+    block_num = div(size(cover)[1], 2)
     path = zeros(Int64, size(cover)[1], h^2)
     y = zeros(Int64, size(cover)[1])
 
@@ -15,7 +18,7 @@ function embed(h_hat, x, m, rho)
             new_weight = zeros(size(weight))
             for k in 1:(2^h)
                 w0 = weight[k] + x[indx]*rho[indx]
-                column = i > limit ? h_hat[1:end-(i-limit),j] : h_hat[:,j]
+                column = i > limit ? reverse(h_hat[1:end-(i-limit),j]) : reverse(h_hat[:,j])
                 ind = ((k-1) ⊻ parse(Int, join(column), base=2))+1
                 w1 = weight[ind] + (1-x[indx])*rho[indx]
                 path[indx, k] = w1 < w0 ? 1 : 0
@@ -25,32 +28,46 @@ function embed(h_hat, x, m, rho)
             weight=new_weight
         end
         for j in 0:2^(h-1)-1
-            #problem here??
             weight[j+1] = weight[2*j + m[indm] + 1]
         end
         [weight[n+1] = Inf for n in 2^(h-1):2^h-1]
         indm += 1
     end
-    println(path)
-    println(weight)
-
     #backward step of viterbi
     embedding_cost = weight[1]
     state = 0
     indx += -1
     indm += -1
     for i in block_num:-1:1
+        state = 2*state + m[indm]
         for j in w:-1:1
             y[indx] = path[indx, (state+1)]
-            column = i > limit ? h_hat[1:end-(i-limit),j] : h_hat[:,j]
+            column = i > limit ? reverse(h_hat[1:end-(i-limit),j]) : reverse(h_hat[:,j])
             state = state ⊻ (y[indx]*parse(Int, join(column), base=2))
             indx += -1
         end
-        state = 2*state + m[indm]
         indm += -1
     end
-    println(embedding_cost)
-    println(y)
+    return(embedding_cost, y)
+end
+
+function expand_h_hat(h_hat, y)
+    h,w = size(h_hat)
+    H = spzeros(Int64, div(size(y)[1],2), size(y)[1])
+    limit = (div(size(y)[1],2))- (h-1)
+    for i in 1:(size(y)[1])
+        o = ceil(Int, i/2)
+        j = mod(i-1,w) +1
+        column = i/2 > limit ? h_hat[1:end-((ceil(Int,i/2))-limit),j] : h_hat[:,j]
+        cutoff = i/2 > limit ? o+h-1-((ceil(Int,i/2)-limit)) : o+h-1
+        H[o:cutoff,i] = column
+    end
+    return H
+end
+
+function extract(h_hat,y)
+    H = expand_h_hat(h_hat,y)
+    show(stdout, "text/plain", H)
 end
 
 #setting up test data
@@ -60,5 +77,7 @@ h_hat = [1 0;
          1 1]
 rho = ones(Int, size(cover))
 
-embed(h_hat, cover, message, rho)
+embedding_cost, y = embed(h_hat, cover, message, rho)
+
+extract(h_hat,y)
 
