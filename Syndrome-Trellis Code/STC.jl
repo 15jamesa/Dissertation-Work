426d0e579ade07af816c88b1480d9fe037f9f96a
Ava <69300596+15jamesa@users.noreply.github.com>
2025-03-25 13:58:28 +0000

    Changed h_hat representation

--- Syndrome-Trellis Code/STC.jl
+++ Syndrome-Trellis Code/STC.jl
@@ -8,10 +8,10 @@ end
 
 #finds the y most similar to x that satisfies Hy = m
 function embed(h_hat, x, m, rho)
-    #change matrix to vector implementation
     #reads columns upwards
     #forward step of viterbi
-    h,w = size(h_hat)
+    w = length(h_hat)
+    h = ndigits(maximum(h_hat), base=2)
     n = length(x)
     block_num = div(n, w)
     path = zeros(Bool, n, h^2)
@@ -27,8 +27,8 @@ function embed(h_hat, x, m, rho)
             new_weight = zero(weight)
             for k in 1:(2^h)
                 w0 = weight[k] + x[indx]*rho[indx]
-                column = i > limit ? reverse(h_hat[1:end-(i-limit),j]) : reverse(h_hat[:,j])
-                ind = ((k-1) ⊻ parse(Int, join(column), base=2))+1
+                column = i > limit ? h_hat[j] & (1 << (h-(i-limit))-1) : h_hat[j]
+                ind = ((k-1) ⊻ column)+1
                 w1 = weight[ind] + (1-x[indx])*rho[indx]
                 path[indx, k] = w1 < w0 ? 1 : 0
                 new_weight[k] = min(w0,w1)
@@ -51,8 +51,8 @@ function embed(h_hat, x, m, rho)
         state = 2*state + m[indm]
         for j in w:-1:1
             y[indx] = path[indx, (state+1)]
-            column = i > limit ? reverse(h_hat[1:end-(i-limit),j]) : reverse(h_hat[:,j])
-            state = state ⊻ (y[indx]*parse(Int, join(column), base=2))
+            column = i > limit ? h_hat[j] & (1 << (h-(i-limit))-1) : h_hat[j]
+            state = state ⊻ (y[indx]*column)
             indx -= 1
         end
         indm -= 1
@@ -60,17 +60,21 @@ function embed(h_hat, x, m, rho)
     return (embedding_cost, y)
 end
 
+
 #create the sparse matrix
 function expand_h_hat(h_hat, y)
-    h,w = size(h_hat)
+    w = length(h_hat)
+    h = ndigits(maximum(h_hat), base=2)
     block_num = div(size(y)[1],w)
     H = spzeros(Int64, block_num, size(y)[1])
     limit = (block_num)-(h-1)
     for i in 1:(size(y)[1])
-        o = ceil(Int, i/w)
+        block = i/w
+        o = ceil(Int, block)
         j = mod(i-1,w) +1
-        column = i/w > limit ? h_hat[1:end-(o-limit),j] : h_hat[:,j]
-        cutoff = i/w > limit ? h-(1-limit) : o+h-1
+        column = block > limit ? h_hat[j] & (1 << (h-(o-limit))-1) : h_hat[j]
+        cutoff = block > limit ? h-(1-limit) : o+h-1
+        column = digits(column, base=2, pad=(cutoff-o+1))
         H[o:cutoff,i] = column
     end
     return H
@@ -79,26 +83,23 @@ end
 #bitwise matrix multiplication
 function extract(h_hat,y)
     H = expand_h_hat(h_hat,y)
-    message = zeros(Int64, 0)
-    for row in eachrow(H)
+    message = zeros(Int64, size(H,1))
+    for (i,row) in enumerate(eachrow(H))
         combined = row .& y
-        ones = count(i->(i==1), combined)
+        ones = sum(combined)
         message_bit = mod(ones,2)==0 ? 0 : 1
-        append!(message, message_bit)
+        message[i] = message_bit
     end
     return(message)
 end
 
 #setting up test data
-cover = [1,0,0,1,1,0,1,0,1,0,0,1,0,1,0]
-message = [0,0,1,0,1]
-h_hat = [1 0 0;
-         1 1 1;
-         1 1 0]
+cover = [0,0,1,1,0,1,0,1,1,1,0,1,1,0,1]
+message = [1,0,0,1,1]
+h_hat = [1,7,6]
 rho = ones(Int, size(cover))
 
 embedding_cost, y = embed(h_hat, cover, message, rho)
 
 println(y)
 println(extract(h_hat,y))
-
